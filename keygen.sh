2062915ac206317ef43daf9ac2fc8694472bdc29
sajidshahriar72543 <sazidshahriar39@gmail.com>
2025-04-03 21:29:43 +0600

    script: restructure the whole script

--- keygen.sh
+++ keygen.sh
@@ -1,29 +1,25 @@
 #!/bin/bash
 
+set -e
+
 echo "This script will generate Android keys for signing builds."
 
-# Define the subject line
 subject='/C=US/ST=California/L=Mountain View/O=Android/OU=Android/CN=Android/emailAddress=android@android.com'
 
-# Print the subject line
-echo "Using Subject Line:"
-echo "$subject"
+echo "Using Subject Line:\n$subject"
 
-# Prompt the user to verify if the subject line is correct
 read -p "Is the subject line correct? (y/n): " confirmation
-
-# Check the user's response
 if [[ $confirmation != "y" && $confirmation != "Y" ]]; then
     echo "Exiting without changes."
     exit 1
 fi
 clear
 
-# Check for existing Android certs and prompt for removal
-if [ -d "$HOME/.android-certs" ]; then
+certs_dir="$HOME/.android-certs"
+if [ -d "$certs_dir" ]; then
     read -p "Existing Android certificates found. Do you want to remove them? (y/n): " remove_confirmation
     if [[ $remove_confirmation == "y" || $remove_confirmation == "Y" ]]; then
-        rm -rf "$HOME/.android-certs"
+        rm -rf "$certs_dir"
         echo "Old Android certificates removed."
     else
         echo "Exiting without changes."
@@ -31,21 +27,21 @@ if [ -d "$HOME/.android-certs" ]; then
     fi
 fi
 
-# Create Key
+mkdir -p "$certs_dir"
 echo "Press ENTER TWICE to skip password (about 10-15 enter hits total). Cannot use a password for inline signing!"
-mkdir ~/.android-certs
 
-for x in bluetooth media networkstack nfc platform releasekey sdk_sandbox shared testkey verifiedboot; do 
-    ./development/tools/make_key ~/.android-certs/$x "$subject"
+keys=(bluetooth media networkstack nfc platform releasekey sdk_sandbox shared testkey verifiedboot)
+for key in "${keys[@]}"; do 
+    ./development/tools/make_key "$certs_dir/$key" "$subject"
 done
 
-# Create vendor directory for keys
-mkdir -p vendor/lineage-priv
-mv ~/.android-certs vendor/lineage-priv/keys
-echo "PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/lineage-priv/keys/releasekey" > vendor/lineage-priv/keys/keys.mk
+vendor_keys_dir="vendor/lineage-priv/keys"
+mkdir -p "$vendor_keys_dir"
+mv "$certs_dir" "$vendor_keys_dir"
 
-# Create BUILD.bazel file
-cat <<EOF > vendor/lineage-priv/keys/BUILD.bazel
+echo "PRODUCT_DEFAULT_DEV_CERTIFICATE := $vendor_keys_dir/releasekey" > "$vendor_keys_dir/keys.mk"
+
+cat <<EOF > "$vendor_keys_dir/BUILD.bazel"
 filegroup(
     name = "android_certificate_directory",
     srcs = glob([
@@ -56,6 +52,7 @@ filegroup(
 )
 EOF
 
-echo "Done! Now build as usual. If builds aren't being signed, add '-include vendor/lineage-priv/keys/keys.mk' to your device mk file"
-echo "Make copies of your vendor/lineage-priv folder or upload it to a private repository as it contains your keys!"
+echo "Done! Now build as usual."
+echo "If builds aren't being signed, add '-include $vendor_keys_dir/keys.mk' to your device mk file."
+echo "Make copies of your $vendor_keys_dir folder or upload it to a private repository as it contains your keys!"
 sleep 3
